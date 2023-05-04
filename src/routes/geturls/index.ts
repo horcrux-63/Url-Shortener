20f08758dc1897583e859c15299dbbe3b9357af2
horcrux-63 <75780336+horcrux-63@users.noreply.github.com>
2023-05-04 13:18:08 +0600

    get, update, delete urls

---
+++ src/routes/geturls/index.ts
@@ -0,0 +1,74 @@
+import { FastifyPluginAsync } from "fastify";
+import isURL = require("is-url");
+
+const getUrls: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.get("/", async function (request, reply) {
+    const client = await fastify.pg.connect();
+    let currentId: number = 6;
+
+    try {
+      const urls = await client.query("select * from urls where id = $1", [
+        currentId,
+      ]);
+      if (urls.rows[0]) {
+        return urls.rows.map((elements: any) => ({
+          shorteneUrl: `${request.protocol}://${request.hostname}/${elements.short_url}`,
+          mainUrl: elements.original_url,
+        }));
+      }
+    } catch (err) {
+      reply.code(400).send("there is an error in the database");
+    } finally {
+      client.release();
+    }
+  });
+
+  fastify.put<{ Params: { id: string } }>(
+    "/:id",
+    async function (request, reply) {
+      const client = await fastify.pg.connect();
+
+      const shortUrl = request.params.id;
+      const newUrl = request.body as string;
+      if (!isURL(newUrl)) {
+        return "invalid url";
+      }
+      try {
+        const updated = await client.query(
+          "update urls set original_url=$1 where short_url =$2",
+          [newUrl, shortUrl]
+        );
+        if (updated["rowCount"]) return "url updated successfully";
+        else return "invalid url";
+      } catch (err) {
+        reply.code(400).send("there is an error in the database");
+      } finally {
+        client.release();
+      }
+    }
+  );
+
+  fastify.delete<{ Params: { id: string } }>(
+    "/:id",
+    async function (request, reply) {
+      const client = await fastify.pg.connect();
+
+      const shortUrl = request.params.id;
+
+      try {
+        const deleted = await client.query(
+          "delete from urls where short_url =$1",
+          [shortUrl]
+        );
+        if (deleted["rowCount"]) return "url deleted successfully";
+        else return "url not found";
+      } catch (err) {
+        reply.code(400).send("there is an error in the database");
+      } finally {
+        client.release();
+      }
+    }
+  );
+};
+
+export default getUrls;
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,48 +2,59 @@ import { FastifyPluginAsync } from "fastify";
 import { nanoid } from "nanoid/async";
 import isURL = require("is-url");
 
-interface Link {
-  id: string;
-  url: string;
+interface userLink {
+  link: string;
+  alias?: string;
 }
-
-const links: Record<string, Link> = {};
-//const links = new Map<string, Link>();
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.post("/", async function (request, reply) {
-    let url: string = request.body as string;
-
-    if (!isURL(url)) {
+    let url: userLink = request.body as userLink;
+    const client = await fastify.pg.connect();
+    if (!isURL(url.link)) {
       reply.status(400).send("Invalid URL");
       return;
     }
-
-    let id = await nanoid(6);
-    const link: Link = { id, url };
-    links[id] = link;
+    let id: string = await nanoid(5);
+    if (url.alias) {
+      id = url.alias;
+    }
+    const date = new Date();
+    const formattedDate = date.toISOString().slice(0, 10);
+    const userId = 6;
+    try {
+      await client.query(
+        "INSERT INTO urls(id,short_url,original_url,expired_date) VALUES ($1, $2, $3, $4)",
+        [userId, id, url.link, formattedDate]
+      );
+    } catch (err) {
+      console.log(err);
+      reply.code(400).send("there is an error in the database");
+    } finally {
+      client.release();
+    }
     return `${request.protocol}://${request.hostname}/${id}`;
   });
-
-  // fastify.post("/login", async function (request, reply) {
-  //   const user: User = request.body as User;
-  //   if (user.userName === "user" && user.password === "pass") {
-  //     reply.send("login successful");
-  //   }
-
-  //   reply.status(404).send("invalid username and password");
-  // });
 
   fastify.get<{ Params: { id: string } }>(
     "/:id",
     async function (request, reply) {
-      const link = links[request.params.id];
+      const client = await fastify.pg.connect();
 
-      if (link) {
-        return reply.redirect(link.url);
+      try {
+        const userinfo = await client.query(
+          "select original_url from urls where short_url = $1",
+          [request.params.id]
+        );
+        const originalUrl = userinfo.rows[0].original_url;
+        if (originalUrl) {
+          return reply.redirect(originalUrl);
+        }
+      } catch (err) {
+        reply.code(400).send("there is an error in the database");
+      } finally {
+        client.release();
       }
-
-      reply.status(404).send();
     }
   );
 
